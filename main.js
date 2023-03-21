ec3e1aed3a3051fda41940f538b8329e38c6becc
AndriiKot <a_kocyuba@ukr.net>
2023-03-21 20:10:37 +0100

    Creation js lesson 2

---
+++ main.js
@@ -0,0 +1,6 @@
+﻿let str = "String 1" // undefined
+str                  // "String 1"
+str.length           //  8
+"Hello js!".length   //  9
+str[0]               // "S"
+str[str.length - 1]  // "1"
